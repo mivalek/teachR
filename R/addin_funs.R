13943963750fc709c8320bb7828e21ee1df2c0dc
mivalek <mivalek@gmail.com>
2022-01-05 06:47:43 +0000

    RStudio addins for teachR divs added

--- R/addin_funs.R
+++ R/addin_funs.R
@@ -21,6 +21,12 @@ insertBox <- function() {
   rstudioapi::setCursorPosition(c(pos + 2, 1))
 }
 
+insertExtraBox <- function() {
+  pos <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range$start[1]
+  rstudioapi::insertText("\n:::{.extra}\n\n:::\n")
+  rstudioapi::setCursorPosition(c(pos + 2, 1))
+}
+
 insertMoreBox <- function() {
   pos <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range$start[1]
   rstudioapi::insertText("\n:::{.more}\n\n:::\n")
@@ -57,5 +63,11 @@ insertQuiz <- function() {
   pos <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range$start[1]
   rstudioapi::insertText("\n```{r, quiz=TRUE}\nmcq(\n\tq = \"\",\n\toptions = c(), # list possible response options,\n\t# default options (can be omitted):\n\tcorrect = 1, # 1st option is correct\n\tshuffle = TRUE, # randomise order of options\n\tcorrect_msg = \"Correct!\",\n\twrong_msg = \"That's not right...\",\n\tnumbered = TRUE #should question have a number?\n)\n\nsaq(\n\tq = \"\",\n\tcorrect = , # provide correct response (numeric or string)\n\t# default options:\n\tnumeric = TRUE, # should user input be interpreted as number or string?\n\tcorrect_msg = \"Correct!\",\n\twrong_msg = \"That's not right...\",\n\tnumbered = TRUE\n)\n```\n")
   rstudioapi::setCursorPosition(c(pos + 3, 7))
+}
+
+insertAside <- function() {
+  pos <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range$start[1]
+  rstudioapi::insertText("\n<aside>\n\n</aside>\n")
+  rstudioapi::setCursorPosition(c(pos + 2, 1))
 }
 
